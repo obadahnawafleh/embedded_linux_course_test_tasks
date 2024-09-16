be6bc1c9a6e9b470433f839d9fa44d84a1649bba
Obadah Nawafleh <obadah.nawafleh@gmail.com>
2024-09-16 14:33:46 +0300

    Delete session_1 directory

--- session_1/task_2.py
+++
@@ -1,9 +0,0 @@
-def isVowel(ch):
- 
-    # Make the list of vowels
-    str = "aeiouAEIOU"
-    return (str.find(ch) != -1)
- 
-# Driver Code
-print('a is '+str(isVowel('a')))
-print('x is '+str(isVowel('x')))
--- session_1/task_4.py
+++
@@ -1,1 +0,0 @@
-from math import pir = float(input("Enter the radius:"))area = pi*r**2print("The area is:", area)
